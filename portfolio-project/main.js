5c21bdc58a84aa589a5275777b0b02f51311a593
mttmrn <52504215+mttmrn@users.noreply.github.com>
2019-09-15 17:31:59 -0400

    Added socials, working on first card

--- portfolio-project/main.js
+++ portfolio-project/main.js
@@ -1,4 +1,60 @@
-document.getElementById('button').addEventListener('click',
+// first card
+document.getElementById('button1').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = "none";
+});
+
+// second card
+document.getElementById('button2').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = "none";
+});
+
+//third card
+document.getElementById('button3').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = "none";
+});
+
+//fourth card
+document.getElementById('button4').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = "none";
+});
+
+//fifth card
+document.getElementById('button5').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click',
+function() {
+    document.querySelector('.bg-modal').style.display = "none";
+});
+
+//sixth card
+document.getElementById('button6').addEventListener('click',
 function() {
     document.querySelector('.bg-modal').style.display = 'flex';
 });
