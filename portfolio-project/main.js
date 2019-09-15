a03e06c3a674af0dc527a258c33acdbf4e328bf4
mttmrn <52504215+mttmrn@users.noreply.github.com>
2019-09-15 18:02:01 -0400

    Finished a little more of the hutch card

--- portfolio-project/main.js
+++ portfolio-project/main.js
@@ -1,65 +1,65 @@
 // first card
 document.getElementById('button1').addEventListener('click',
 function() {
-    document.querySelector('.bg-modal').style.display = 'flex';
+    document.querySelector('.bg-modal1').style.display = 'flex';
 });
 
-document.querySelector('.close').addEventListener('click',
+document.querySelector('.close1').addEventListener('click',
 function() {
-    document.querySelector('.bg-modal').style.display = "none";
+    document.querySelector('.bg-modal1').style.display = "none";
 });
 
 // second card
 document.getElementById('button2').addEventListener('click',
 function() {
-    document.querySelector('.bg-modal').style.display = 'flex';
+    document.querySelector('.bg-modal2').style.display = 'flex';
 });
 
-document.querySelector('.close').addEventListener('click',
+document.querySelector('.close2').addEventListener('click',
 function() {
-    document.querySelector('.bg-modal').style.display = "none";
+    document.querySelector('.bg-modal2').style.display = "none";
 });
 
 //third card
 document.getElementById('button3').addEventListener('click',
 function() {
-    document.querySelector('.bg-modal').style.display = 'flex';
+    document.querySelector('.bg-modal3').style.display = 'flex';
 });
 
-document.querySelector('.close').addEventListener('click',
+document.querySelector('.close3').addEventListener('click',
 function() {
-    document.querySelector('.bg-modal').style.display = "none";
+    document.querySelector('.bg-modal3').style.display = "none";
 });
 
 //fourth card
 document.getElementById('button4').addEventListener('click',
 function() {
-    document.querySelector('.bg-modal').style.display = 'flex';
+    document.querySelector('.bg-modal4').style.display = 'flex';
 });
 
-document.querySelector('.close').addEventListener('click',
+document.querySelector('.close4').addEventListener('click',
 function() {
-    document.querySelector('.bg-modal').style.display = "none";
+    document.querySelector('.bg-modal4').style.display = "none";
 });
 
 //fifth card
 document.getElementById('button5').addEventListener('click',
 function() {
-    document.querySelector('.bg-modal').style.display = 'flex';
+    document.querySelector('.bg-modal5').style.display = 'flex';
 });
 
-document.querySelector('.close').addEventListener('click',
+document.querySelector('.close5').addEventListener('click',
 function() {
-    document.querySelector('.bg-modal').style.display = "none";
+    document.querySelector('.bg-modal5').style.display = "none";
 });
 
 //sixth card
 document.getElementById('button6').addEventListener('click',
 function() {
-    document.querySelector('.bg-modal').style.display = 'flex';
+    document.querySelector('.bg-modal6').style.display = 'flex';
 });
 
-document.querySelector('.close').addEventListener('click',
+document.querySelector('.close6').addEventListener('click',
 function() {
-    document.querySelector('.bg-modal').style.display = "none";
+    document.querySelector('.bg-modal6').style.display = "none";
 });
