3efe7dabd28dea175d9da3641b8a2b431ad20b81
Matt Moran <mattmoran50@gmail.com>
2019-10-07 18:23:03 -0400

    Added car search project + added resume

--- public/main.js
+++ public/main.js
@@ -1,65 +1,65 @@
 // first card
 document.getElementById('button1').addEventListener('click',
-function() {
-    document.querySelector('.bg-modal1').style.display = 'flex';
-});
+    function () {
+        document.querySelector('.bg-modal1').style.display = 'flex';
+    });
 
 document.querySelector('.close1').addEventListener('click',
-function() {
-    document.querySelector('.bg-modal1').style.display = "none";
-});
+    function () {
+        document.querySelector('.bg-modal1').style.display = "none";
+    });
 
 // second card
 document.getElementById('button2').addEventListener('click',
-function() {
-    document.querySelector('.bg-modal2').style.display = 'flex';
-});
+    function () {
+        document.querySelector('.bg-modal2').style.display = 'flex';
+    });
 
 document.querySelector('.close2').addEventListener('click',
-function() {
-    document.querySelector('.bg-modal2').style.display = "none";
-});
+    function () {
+        document.querySelector('.bg-modal2').style.display = "none";
+    });
 
 //third card
 document.getElementById('button3').addEventListener('click',
-function() {
-    document.querySelector('.bg-modal3').style.display = 'flex';
-});
+    function () {
+        document.querySelector('.bg-modal3').style.display = 'flex';
+    });
 
 document.querySelector('.close3').addEventListener('click',
-function() {
-    document.querySelector('.bg-modal3').style.display = "none";
-});
+    function () {
+        document.querySelector('.bg-modal3').style.display = "none";
+    });
 
 //fourth card
 document.getElementById('button4').addEventListener('click',
-function() {
-    document.querySelector('.bg-modal4').style.display = 'flex';
-});
+    function () {
+        document.querySelector('.bg-modal4').style.display = 'flex';
+    });
 
 document.querySelector('.close4').addEventListener('click',
-function() {
-    document.querySelector('.bg-modal4').style.display = "none";
-});
+    function () {
+        document.querySelector('.bg-modal4').style.display = "none";
+    });
 
 //fifth card
 document.getElementById('button5').addEventListener('click',
-function() {
-    document.querySelector('.bg-modal5').style.display = 'flex';
-});
+    function () {
+        document.querySelector('.bg-modal5').style.display = 'flex';
+    });
 
 document.querySelector('.close5').addEventListener('click',
-function() {
-    document.querySelector('.bg-modal5').style.display = "none";
-});
+    function () {
+        document.querySelector('.bg-modal5').style.display = "none";
+    });
 
 //sixth card
 document.getElementById('button6').addEventListener('click',
-function() {
-    document.querySelector('.bg-modal6').style.display = 'flex';
-});
+    function () {
+        document.querySelector('.bg-modal6').style.display = 'flex';
+    });
 
 document.querySelector('.close6').addEventListener('click',
-function() {
-    document.querySelector('.bg-modal6').style.display = "none";
-});
+    function () {
+        document.querySelector('.bg-modal6').style.display = "none";
+    });
